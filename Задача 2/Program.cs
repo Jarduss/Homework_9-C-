b91d72c2070230878ce2262a21fd21f2d3520b41
danil <jardus@bk.ru>
2022-10-30 16:01:00 +0300

    HomeWork_9

---
+++ Задача 2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+
+System.Console.Write("Введите A ");
+int A = int.Parse(Console.ReadLine());
+System.Console.Write("Введите B ");
+int B = int.Parse(Console.ReadLine());
+void getNumbersMN(int A, int B, int sum)
+{
+    if (A>= B)
+    {
+        if (A == B) 
+        {
+            System.Console.WriteLine(A);
+            System.Console.WriteLine(" ");
+            System.Console.WriteLine($"Сумма чисел в промежутке от числа А до числа В составляет: {sum + A}");
+            return;
+        }
+        else 
+        {
+        sum = sum + A;
+        System.Console.WriteLine(A);
+        A-=1;     
+        getNumbersMN(A,B,sum);
+        }
+    }
+    else System.Console.WriteLine($"{B} > {A} введите правильные данные");
+} 
+getNumbersMN(A,B, 0);
---
+++ Задача 3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+System.Console.Write("Введите M ");
+int M = int.Parse(Console.ReadLine());
+System.Console.Write("Введите N ");
+int N = int.Parse(Console.ReadLine());
+
+int Akkerman = Formula(M, N);
+
+Console.Write($"Результат вычисления функции Аккермана = {Akkerman} ");
+
+int Formula(int M, int N)
+{
+  if (M == 0) return N + 1;
+  else if (N == 0) return Formula(M - 1, 1);
+  else return Formula(M - 1, Formula(M, N - 1));
+}
+
